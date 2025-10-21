39d024aa0e18759baab11c493b7fe45a3d5335c1
aymensellaouti <aymen.sellaouti@gmail.com>
2025-10-21 11:47:12 +0100

    slider

--- src/app/rxjs/slider/slider.component.ts
+++ src/app/rxjs/slider/slider.component.ts
@@ -17,5 +17,9 @@ export class SliderComponent {
   ];
 
   /* Todo : Créer le flux permettant de générer les images à afficher dans le slider */
-  paths$!: Observable<string>;
+  paths$: Observable<string> = timer(0, this.timer).pipe(
+    // 0 1 2 3 4 5 6 7
+    map(index => this.imagePaths[index % this.imagePaths.length])
+    // img1 img2 img3 ....
+  );
 }
