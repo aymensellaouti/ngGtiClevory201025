be1c04598b46bc17f170ee43a5679b33c8fddad1
aymensellaouti <aymen.sellaouti@gmail.com>
2025-10-21 14:41:02 +0100

    start auth

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -4,11 +4,16 @@ import { LoginResponseDto } from '../dto/login-response.dto';
 import { HttpClient } from '@angular/common/http';
 import { API } from '../../../config/api.config';
 import { Observable } from 'rxjs';
+import { CONSTANTES } from 'src/config/const.config';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+
+  user$ // flux eli traja3 ya connectedUser kan connecté sinon null
+  isLoggedIn$!: Observable<boolean>;
+  isLoggedOut!: Observable<boolean>;
   constructor(private http: HttpClient) {}
 
   login(credentials: CredentialsDto): Observable<LoginResponseDto> {
@@ -22,4 +27,19 @@ export class AuthService {
   logout() {
     localStorage.removeItem('token');
   }
+  saveToken(token: string) {
+    localStorage.setItem(CONSTANTES.token, token);
+  }
+
+  getToken(): string {
+    return localStorage.getItem(CONSTANTES.token) ?? '';
+  }
+
+  removeToken() {
+    localStorage.removeItem(CONSTANTES.token);
+  }
+}
+
+export class ConnectedUser {
+  constructor(public id: number, public email: string) {}
 }
--- src/config/const.config.ts
+++ src/config/const.config.ts
@@ -1,4 +1,5 @@
 export const CONSTANTES = {
   defaultImage: "cv.png",
   env: "prod",
+  token: "token"
 };
