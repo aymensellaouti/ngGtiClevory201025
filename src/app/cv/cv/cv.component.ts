4b2f481457f880e7e5341a22d2d7591ef3c3b2ea
aymensellaouti <aymen.sellaouti@gmail.com>
2025-10-21 13:57:31 +0100

    delete input output communication

--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -3,7 +3,7 @@ import { Cv } from "../model/cv";
 import { LoggerService } from "../../services/logger.service";
 import { ToastrService } from "ngx-toastr";
 import { CvService } from "../services/cv.service";
-import { EMPTY, Observable, catchError, of } from "rxjs";
+import { EMPTY, Observable, catchError, of, retry } from "rxjs";
 import { TodoService } from "src/app/todo/service/todo.service";
 @Component({
   selector: 'app-cv',
@@ -13,6 +13,10 @@ import { TodoService } from "src/app/todo/service/todo.service";
 export class CvComponent {
   //cvs: Cv[] = [];
   cvs$ = this.cvService.getCvs().pipe(
+    retry({
+      delay: 2000,
+      count: 2
+    }),
     catchError(
       e => {
          this.toastr.error(`
