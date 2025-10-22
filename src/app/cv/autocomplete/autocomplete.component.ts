30220832fafa3d5938e2119676b9f767874950a1
aymensellaouti <aymen.sellaouti@gmail.com>
2025-10-22 10:24:13 +0100

    start autocomplete

--- src/app/cv/autocomplete/autocomplete.component.ts
+++ src/app/cv/autocomplete/autocomplete.component.ts
@@ -15,4 +15,9 @@ export class AutocompleteComponent {
     return this.form.get("search")!;
   }
   form = this.formBuilder.group({ search: [""] });
+  constructor() {
+    this.search.valueChanges.pipe(
+      tap(value => console.log(value))
+    ).subscribe()
+  }
 }
